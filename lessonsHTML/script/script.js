c4e01f133b7b8af28db308f12b86d207806757fb
DmitriyM <mishchukltd@gmail.com>
2020-07-23 21:23:49 +0300

    Insert onclick on html

---
+++ lessonsHTML/script/script.js
@@ -0,0 +1,7 @@
+
+
+function ourFunction() {
+    let paragraf = document.querySelector('#paragarf');
+    let text = "<h1>Привет мир.</h1> Я Рой Миллер. Я спасаю мир от плолхих парней, но сегодня я отдыхаю вместе с Вами. Приятного просмотра";
+    paragraf.innerHTML = paragraf.textContent + text;
+}
