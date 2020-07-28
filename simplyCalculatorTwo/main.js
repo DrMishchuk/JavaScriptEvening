de0cbc871f9d5bdca70f014c8cd16ffc731bd764
DmitriyM <mishchukltd@gmail.com>
2020-07-29 01:09:04 +0300

    simlyCalculatorTwo have been finished

--- simplyCalculatorTwo/main.js
+++ simplyCalculatorTwo/main.js
@@ -1,24 +1,39 @@
-let numberOne = document.getElementById('numberOne').value;
-let numberTwo = document.getElementById('numberTwo').value;
-
-let buttonPlus = document.getElementById('buttonPlus');
-let buttonMinus = document.getElementById('buttonMinus');
-
-buttonPlus.addEventListener('click', Plus);
-buttonMinus.addEventListener('click', Minus);
-
-function Plus() {
-    let numberOne = Number(document.getElementById('numberOne').value);
-    let numberTwo = Number(document.getElementById('numberTwo').value);
-    document.getElementById('output').innerHTML = plus(numberOne, numberTwo);
+let buttons = document.getElementsByTagName('button');
+for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener('click', buttonClick);  
 }
 
-function Minus() {
-    let numberOne = Number(document.getElementById('numberOne').value);
-    let numberTwo = Number(document.getElementById('numberTwo').value);
-    document.getElementById('output').innerHTML = minus(numberOne, numberTwo);
+function viewResult(resultStrig){
+    document.getElementById('output').innerHTML = resultStrig;
 }
 
+function buttonClick (event) {
+    controler(event.currentTarget.innerHTML);
+}
+
+function controler(eventTitle){
+    let numberOne = Number(document.getElementById('numberOne').value);
+    let numberTwo = Number(document.getElementById('numberTwo').value);
+    
+    if (eventTitle === '+'){
+        viewResult(plus(numberOne, numberTwo));
+    }
+    else if (eventTitle === '-'){
+        viewResult(minus(numberOne, numberTwo));
+    }
+    else if (eventTitle === '*'){
+        viewResult(miltiply(numberOne, numberTwo));
+    }
+    else if (eventTitle === '/'){
+        viewResult(devide(numberOne, numberTwo));
+    }
+    else if (eventTitle === 'C'){
+        viewResult("<h1>Hello</h1>");
+    }
+    else {
+        console.log('Error');
+    }
+}
 
 function plus(numberOne, numberTwo) {
     return numberOne + numberTwo;
