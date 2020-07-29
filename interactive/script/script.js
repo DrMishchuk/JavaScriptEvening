8a22ac840e8ab786a3291fa00e6d056217c7ca1d
DmitriyM <mishchukltd@gmail.com>
2020-07-29 22:59:11 +0300

    interactiveJS

---
+++ interactive/script/script.js
@@ -0,0 +1,17 @@
+console.log('hello1');
+alert();
+let printTextId = setTimeout(printText, 1);
+console.log('hello2');
+
+for (let index = 0; index < 1000; index++) {
+    console.log(1);;
+    
+}
+console.log('hello2');
+
+
+
+function printText(param) {
+    console.log('hello1');
+}
+
